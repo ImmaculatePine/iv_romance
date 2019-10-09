f673ef1ab1906c6c7cd2147fdf765f6a89a5b2d5
Alexander Borovykh <immaculate.pine@gmail.com>
2019-10-09 23:04:10 +0200

    Fix all strict credo issues

--- .credo.exs
+++ .credo.exs
@@ -4,10 +4,20 @@
       name: "default",
       files: %{
         included: ["lib/", "test/"],
-        excluded: []
+        excluded: ["test/support/*_case.ex"]
       },
       checks: [
         {Credo.Check.Readability.ModuleDoc, false}
+      ]
+    },
+    %{
+      name: "support",
+      files: %{
+        included: ["test/support/*_case.ex"],
+        excluded: []
+      },
+      checks: [
+        {Credo.Check.Design.AliasUsage, false}
       ]
     }
   ]
